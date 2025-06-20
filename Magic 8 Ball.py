b3ff3e2285ac982f4bdc184c5ce73742db168759
imtiffanyng <tiffanyngno@gmail.com>
2025-06-20 12:45:58 -0500

    Add files via upload

---
+++ Magic 8 Ball.py
@@ -0,0 +1,25 @@
+# write a program that shows random magic 8 ball responses
+#
+# define file name
+magic8 = '/Users/tiffanynguyen/Documents/8ballresponses.txt'
+# verify that the file can be read
+try:
+    with open(magic8, 'r') as file:
+        responses = [line.strip() for line in file.readlines()]
+except FileNotFoundError:
+    print(f'error, file {magic8} not found.')
+    exit()
+#
+# main program
+# ask user for input
+import random
+#
+while True:
+    input('please ask a (yes/no) question: ')
+    answer = random.choice(responses)
+    print('magic 8 Ball says:', answer)
+    again = input('do you want to ask another question? (yes/no): ')
+    if again.lower() not in ['y', 'yes']:
+        break
+#
+print('program ends.')
